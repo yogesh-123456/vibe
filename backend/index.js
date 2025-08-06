1a66bc617b418543290bb0f005caaf8a40f5f5c4
yogesh-123456 <167056322+yogesh-123456@users.noreply.github.com>
2025-08-06 08:28:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ dotenv.config()
 
 const port=process.env.PORT || 5000
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://vibe-frontend-ladh.onrender.com",
     credentials:true
 }))
 app.use(express.json())
